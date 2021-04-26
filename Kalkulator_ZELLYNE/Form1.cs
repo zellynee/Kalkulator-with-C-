c51cb88375bb4171c68b1a3f2bcecf8496ef907f
zellynee <zellyneoctavianii234@gmail.com>
2021-04-26 10:14:28 +0700

    kalkulator

---
+++ Kalkulator_ZELLYNE/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_ZELLYNE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            LBLHasil.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 harus diisi terlebih dahulu ya!!");
+            }
+            else
+            {
+                float a, b, c;
+                a = float.Parse(this.textBox1.Text);
+                b = float.Parse(this.textBox2.Text);
+                c = a + b;
+                this.LBLHasil.Text = c.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            LBLHasil.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 harus diisi terlebih dahulu ya!!");
+            }
+            else
+            {
+                float a, b, c;
+                a = float.Parse(this.textBox1.Text);
+                b = float.Parse(this.textBox2.Text);
+                c = a - b;
+                this.LBLHasil.Text = c.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 harus diisi terlebih dahulu ya!!");
+            }
+            else
+            {
+                float a, b, c;
+                a = float.Parse(this.textBox1.Text);
+                b = float.Parse(this.textBox2.Text);
+                c = a * b;
+                this.LBLHasil.Text = c.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 harus diisi terlebih dahulu ya!!");
+            }
+            else
+            {
+                float a, b, c;
+                a = float.Parse(this.textBox1.Text);
+                b = float.Parse(this.textBox2.Text);
+                c = a / b;
+                this.LBLHasil.Text = c.ToString();
+            }
+        }
+    }
+}
